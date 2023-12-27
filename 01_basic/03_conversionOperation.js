ae6980731c300e0e53f41df20c87ec22b797f43c
Amit Dewal <108610826+amitdewal@users.noreply.github.com>
2023-12-27 18:40:35 +0000

    data type conversion in js

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33";
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+
+// "33" => 33
+//"33abc" => NaN
+//true => 1;
+//""   => false
+//false => 
+//null => false;
+//NaN  => false; 
+
+let isLoggedIn = NaN;
+//boolean conversion
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//string conversion
+let someValue = 100;
+let StringValue = String(someValue);
+console.log(typeof StringValue);
+console.log(StringValue);
