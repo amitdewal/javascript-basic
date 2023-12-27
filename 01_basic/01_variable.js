50930a9c92bc6b5c16b5c5b7c463d72fd179371f
Amit Dewal <108610826+amitdewal@users.noreply.github.com>
2023-12-27 18:10:27 +0000

    related to the js variable declaration

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = 1234;//constant defining in js
+let accountEmail = "amitdewal@mail.com"
+var accountPassword = "1233";
+accountCity = 'delhi';
+let accountState;
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
