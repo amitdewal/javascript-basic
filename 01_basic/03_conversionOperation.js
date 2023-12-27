74ee6740497a2d19b7c72eb82330a8a941925375
Amit Dewal <108610826+amitdewal@users.noreply.github.com>
2023-12-27 18:58:00 +0000

    conversion operation always try mdn doc if find confusion during conversion of js data types

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -17,10 +17,26 @@ let score = "33";
 let isLoggedIn = NaN;
 //boolean conversion
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 //string conversion
 let someValue = 100;
 let StringValue = String(someValue);
-console.log(typeof StringValue);
-console.log(StringValue);
+// console.log(typeof StringValue);
+// console.log(StringValue);
+
+//*******************operations*********************//
+
+let value = 3;
+let negValue = -value;//negative value;
+//console.log(negValue);
+
+let str1 = "hello";
+let str2 = " world";
+let str3 = str1 + str2;
+console.log(str3);
+
+
+console.log("2" + 1);//if string is first then first string concate
+console.log(2 + "1");//
+console.log(2 + 1+ "1");//if number is first then addition is first then concate
