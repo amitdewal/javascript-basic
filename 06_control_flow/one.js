6158ed0180303d15acbd8df8cc561485776c8aad
Amit Dewal <daredewal007@gmail.com>
2023-12-29 01:10:56 +0530

    controller flow statement

---
+++ 06_control_flow/one.js
@@ -0,0 +1,48 @@
+//if else while for do-while 
+// const isUserLoggedIn = true;
+// const temperature = 41;
+// if(temperature === 40){
+//    console.log("less than 50");
+// }else{
+//     console.log("greter than 50")
+// }
+
+ // <, >, <=, >=, ==, !=,===
+
+//  const score = 200;
+//  if(score > 100){
+//     const power = 'fly';
+//     console.log(`user power: ${power}`);
+// }
+//console.log(`user power: ${power}`);//error out of scope
+
+
+ const balance = 1000;
+// if(balance > 500) console.log("test");
+
+// if(balance < 500){
+//     console.log("less than ")
+// }else if(balance < 750){
+//     console.log("less than 750");
+// }else if(balance < 900){
+//     console.log("less than 750");
+// }else{
+//     console.log("less than 1200");
+// }
+
+
+//
+// const isUserLoggedIn = true;
+// const debitCard = true;
+// const loggedInFromGoogle = false;
+// const loggedInFromEmail = true;
+
+// if(isUserLoggedIn && debitCard  ){
+//     console.log("allow to buy course");
+// }
+
+// if(isUserLoggedIn || loggedInFromEmail  ){
+//     console.log("user logged in");
+// }
+
+
