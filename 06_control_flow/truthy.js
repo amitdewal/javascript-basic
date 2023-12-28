00e127238855cabff3578d2b8320d421509c2fb7
Amit Dewal <daredewal007@gmail.com>
2023-12-29 01:11:51 +0530

    truthsy and falsy value and ternary operator and nullish  coalescing Operator

---
+++ 06_control_flow/truthy.js
@@ -0,0 +1,40 @@
+const userEmail = "";
+
+// if(userEmail){
+//     console.log("got user email")
+// }else{
+//     console.log("dont have user email");
+// }
+
+// falsy value are given below
+// false, 0, -0, bigInt 0n, "", null, undefined,NaN
+
+//truthy value
+// "0", 'false'," ", [], {}, function(){}
+
+// if(userEmail.length ===0){
+//     console.log("Array is empty");
+// }
+
+// const emptyObj = {};
+// if(Object.keys(emptyObj).length === 0){
+//     console.log("object is empty");
+// }
+
+
+//nullish  coalescing Operator (??): null underfined same as NVL function of Oracle Db
+
+let val1;
+//val1 = 5 ?? 10;
+//val1 = null ?? 10;
+val1 = undefined ?? 10;
+
+//console.log(val1);//10
+
+//ternary operator
+
+//conditon ? true : false;
+
+// const iceTeaPrice = 81;
+// iceTeaPrice >= 80 ? console.log("yes more  80") : console.log(" less than 80");
+
