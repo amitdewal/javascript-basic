fd4ab0dd351924e91ec87398257e4c5706fcf303
Amit Dewal <daredewal007@gmail.com>
2023-12-28 18:59:55 +0530

    rearrange the file into the folder

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+console.log("amit dewal");
+console.log("amit kumar");
+
+//data types
+
+let name = "amit";
+let age = 18;
+let isLogged = true;
+console.table([name, age, isLogged]);
--- test.js
+++
@@ -1,9 +0,0 @@
-console.log("amit dewal");
-console.log("amit kumar");
-
-//data types
-
-let name = "amit";
-let age = 18;
-let isLogged = true;
-console.table([name, age, isLogged]);
