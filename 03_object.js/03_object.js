57d78320e4ba476d60bee21eda0cfd767b203680
Amit Dewal <daredewal007@gmail.com>
2023-12-28 20:32:46 +0530

    object part 3

---
+++ 03_object.js/03_object.js
@@ -0,0 +1,32 @@
+//part 3 of object
+
+const course = {
+    courseName : "js in hindi",
+    price : "999",
+    courseInstrcture:"amit"
+}
+
+const {courseInstrcture : instructor} = course;// way to access the element in the js mostly used in the react
+console.log( instructor);
+
+// const navbar = ({company}) =>{
+
+// }
+// navbar(company = "amit");
+
+
+//api calling in js 
+
+
+// //JSON
+// {
+//     "name" : "amit",
+//     "courseName" :"free"
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
+
