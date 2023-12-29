3cf63bcd717e5427aff92755bba7a0ab24406376
Amit Dewal <daredewal007@gmail.com>
2023-12-29 10:19:05 +0530

    do while and while loop

--- 07_loops/loops.js
+++ 07_loops/loops.js
@@ -32,12 +32,41 @@
 
 // continue, break some keywords in java
 
-for (let index = 1; index <= 20; index++) {
-    if(index == 5){
-        console.log(` 5 detected`);
-    //    break; // stop the loop
-    //    continue;// jump the iteration
-    }
-    console.log(`value of i is ${index}`);
+// for (let index = 1; index <= 20; index++) {
+//     if(index == 5){
+//         console.log(` 5 detected`);
+//     //    break; // stop the loop
+//     //    continue;// jump the iteration
+//     }
+//    // console.log(`value of i is ${index}`);
     
+// }
+
+
+// while loop, do-while loop
+
+let index = 0;
+
+while (index <= 10) {
+    //console.log(`value of index is ${index}`);
+    index = index + 2;
 }
+
+let myArray = ["batman", "superman", "flash"];
+
+let arr = 0;
+while(arr < myArray.length){
+  //  console.log(`value is ${myArray[arr]}`);
+    arr++;
+}
+
+// do while loop
+
+// let score = 11;
+// do {
+//     console.log(`score is ${score}`);
+//     score ++;
+// } while (score <= 10);
+
+// loop specially for array
+
