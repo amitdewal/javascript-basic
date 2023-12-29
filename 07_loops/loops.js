0256782791464bb919d30aee3cd7f02be1021d66
Amit Dewal <daredewal007@gmail.com>
2023-12-29 10:11:00 +0530

    for loops

---
+++ 07_loops/loops.js
@@ -0,0 +1,43 @@
+//loops in js for while do-while loops
+
+// for (let index = 0; index <= 10; index++) {
+//    const element = index;
+//     if(element == 5){
+//         console.log("5 is best number");
+        
+//     }
+    
+//     console.log(element);
+// }
+
+//out of scope;
+//console.log(index);
+
+// nested loop
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`outer loop ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`inner loop value ${j} and outer loop ${i}`)
+        
+//     }
+// }
+
+// let myArr = ["flash","batman","superman"];
+// console.log(myArr.length);
+// for (let index = 0; index < myArr.length; index++) {
+//     const element = myArr[index];
+//     console.log(element);
+    
+// }
+
+// continue, break some keywords in java
+
+for (let index = 1; index <= 20; index++) {
+    if(index == 5){
+        console.log(` 5 detected`);
+    //    break; // stop the loop
+    //    continue;// jump the iteration
+    }
+    console.log(`value of i is ${index}`);
+    
+}
